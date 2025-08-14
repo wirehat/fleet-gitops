62613834434a5467877f3b49adf499442c9503f2
Justin LaBo <justin.labo@benchling.com>
2025-08-14 13:58:39 -0700

    updating files

--- lib/gatekeeper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Enable Gatekeeper
-spctl --master-enable
-
-# Verify status
-spctl --status
--- lib/zoom-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Configuring Zoom! Here's a chicken 
-   \\
-   (o>
-\\_//)
- \_/_)
-  _|_"
